7d8ba4eb051778b7fd42be4fba64fe281ab58586
rijaramalanjaona <rijamamitiana.ramalanjaona@ingesup.com>
2019-10-01 13:23:27 +0200

    utilisation du composant dans une reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+	form: FormGroup;
+
+	constructor(private fb: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.form = this.fb.group({
+			counter: 6
+		});
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { CounterInputComponent } from './counter-input/counter-input.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterInputComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CounterInputComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
